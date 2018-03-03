4c37dd68f1e37158bb6b20336ad4e496af26956f
Christophe Simon <c.christophesimon@gmail.com>
2018-03-03 14:00:18 +0100

    Add class Pane

---
+++ KitBoxApp/KitBoxApp/Pane.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KitBoxApp
+{
+    class Pane
+    {
+        private string color;
+        private int price;
+        private string type;
+
+        public Pane(string color, int price, string type)
+        {
+            this.color = color;
+            this.price = price;
+            this.type = type;
+        }
+
+        public int GetPrice(int height)
+        {
+            return this.price; // How to integer the height with our class diagram ?
+        }
+        public string Type
+        {
+            get => type;
+        }
+    }
+}
