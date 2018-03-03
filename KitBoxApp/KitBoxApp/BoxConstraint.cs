ad90f83281dfdb910155711b25ff1494f13c6c88
Christophe Simon <c.christophesimon@gmail.com>
2018-03-03 14:54:06 +0100

    Add class BoxConstraint

---
+++ KitBoxApp/KitBoxApp/BoxConstraint.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KitBoxApp
+{
+    class BoxConstraint : IConstraintChecker
+    {
+        private List<int> heights;
+        private List<string> colors;
+
+        public BoxConstraint(List<int> heights, List<string> colors)
+        {
+            this.heights = heights;
+            this.colors = colors;
+        }
+
+        public bool check<T>(Box b)  //How to check this part and resolve the problem of generic type ?
+        {
+            return true;
+        }
+    }
+}
--- KitBoxApp/KitBoxApp/CupboardConstraint.cs
+++ KitBoxApp/KitBoxApp/CupboardConstraint.cs
@@ -23,6 +23,5 @@ namespace KitBoxApp
         {
             return true;
         }
-}
     }
 }
