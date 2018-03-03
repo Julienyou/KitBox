a250861694df8e55ab281cacdddd2338a108e66b
Christophe Simon <c.christophesimon@gmail.com>
2018-03-03 14:43:04 +0100

    Add CupboardConstraint

---
+++ KitBoxApp/KitBoxApp/CupboardConstraint.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KitBoxApp
+{
+    class CupboardConstraint : IConstraintChecker
+    {
+        private List<int> dephts;
+        private List<int> widths;
+        private int maxHeight;
+
+        public CupboardConstraint(List<int> dephts, List<int> widths, int maxHeight)
+        {
+            this.dephts = dephts;
+            this.widths = widths;
+            this.maxHeight = maxHeight;
+        }
+
+        public bool Check<T>(Cupboard cb)  //How to check this part and resolve the problem of generic type ?
+        {
+            return true;
+        }
+}
+    }
+}
