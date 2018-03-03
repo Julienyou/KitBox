8b27f0351e341dbc00b08114fa0c8f45fbd0edf6
Christophe Simon <c.christophesimon@gmail.com>
2018-03-03 15:00:28 +0100

    Add class DoorConstraint

---
+++ KitBoxApp/KitBoxApp/DoorConstraint.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KitBoxApp
+{
+    class DoorConstraint : IConstraintChecker
+    {
+        private bool knop;
+        private List<string> colors;
+        private List<Tuple<int>> doorDimensions;
+
+        public DoorConstraint(bool knop, List<string> colors, List<Tuple<int>> doorDimensions)
+        {
+            this.knop = knop;
+            this.colors = colors;
+            this.doorDimensions = doorDimensions;
+        }
+
+        public bool check<T>(Door door)  //How to check this part and resolve the problem of generic type ?
+        {
+            return true;
+        }
+    }
+}
