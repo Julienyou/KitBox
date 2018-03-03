c2496a7dc752bbc1dd7e421bf0eb77d1635e666f
Christophe Simon <c.christophesimon@gmail.com>
2018-03-03 15:37:35 +0100

    Add class Box

---
+++ KitBoxApp/KitBoxApp/Box.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KitBoxApp
+{
+    class Box
+    {
+        private int height;
+        private List<IComponent> components;
+        private Tuple<int> boxDimensions;
+        private int price;
+        private List<Pane> panes;
+
+        public Box(int height, List<IComponent> components, Tuple<int> boxDimensions, int price, List<Pane> panes)
+        {
+            this.height = height;
+            this.components = components;
+            this.boxDimensions = boxDimensions;
+            this.price = price;
+            this.panes = panes;
+        }
+
+        public int Price
+        {
+            get => price;
+        }
+
+        public void AddComponent(IComponent c)
+        {
+            components.Add(c);
+        }
+
+        public void RemoveComponent(IComponent c)
+        {
+            components.Remove(c);
+        }
+
+        public Tuple<int> BoxDimensions
+        {
+            get => boxDimensions;
+        }
+    }
+}
