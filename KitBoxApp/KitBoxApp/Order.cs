c3c6970c7738d085c478bfaf4d65685fb574a0b3
Ludovic Merel <Ludo.m@live.fr>
2018-03-12 18:27:17 +0100

    Creat Utils.cs and correction Order.cs/OrderDataBase.mdj

--- KitBoxApp/KitBoxApp/Order.cs
+++ KitBoxApp/KitBoxApp/Order.cs
@@ -14,19 +14,12 @@ namespace KitBoxApp
         private Customer customer = null;
         private List<Component> components;
 
-        /*Init variables for communicate with the DataBase*/
-        private SQLiteConnection dbConnection;
-
         /*classes not yet created*/
         //private State state; 
 
         public Order(string id)
         {
             this.id = id;
-
-            /*Start connection DataBase*/
-            dbConnection = new SQLiteConnection("Data Source=db.sqlite;Version=3;");
-            dbConnection.Open();
         }
 
         public string Id
@@ -39,9 +32,24 @@ namespace KitBoxApp
             }
         }
 
+        public int TotalPrice
+        {
+            get => totalPrice;
+
+            set
+            {
+                totalPrice = value;
+            }
+        }
+
         public Customer Customer
         {
             get => Customer;
+        }
+
+        public List<Component> Components
+        {
+            get => components;
         }
 
         public void ComputePrice()
@@ -61,65 +69,11 @@ namespace KitBoxApp
         public void SetCustomer(string email, string firstName, string lastName, string street, string town)
         {
             customer = new Customer(email, firstName, lastName, street, town);
-        }        
-
-        public void ExportToDatabase()
-        {
-            CheckCustomer();
-            CheckOrderId();
-
-            /*If the customer and the id to order are good*/
-            string sql = "insert into `Order` ('PK_IDOrder', 'FK_Customer', 'FK_State', 'TotalPrice')" +
-                         "values ('" + id + "','" + customer.Email + "','" + "1" + "','" + totalPrice.ToString() + "')";
-
-            SQLiteCommand command = new SQLiteCommand(sql, dbConnection);
-            command.ExecuteNonQuery();
-
-
-            /*Add data inOrderComponentLink for each component located in components*/
-            foreach (Component component in components)
-            {
-                sql = "insert into `OrderComponentLink` ('FK_Order', 'FK_Component')" +
-                         "values ('" + id + "','" + component.code + "')";
-
-                command = new SQLiteCommand(sql, dbConnection);
-                command.ExecuteNonQuery();
-            }
-
-            dbConnection.Close();            
         }
 
-        private void CheckCustomer()
+        public void AddComponent(Component component)
         {
-            /*Verification if the customer exists*/
-            string sql = "select * from Customer where PK_Email='" + customer.Email + "'";
-
-            SQLiteCommand command = new SQLiteCommand(sql, dbConnection);
-            SQLiteDataReader reader = command.ExecuteReader();
-
-            if (!reader.Read()) //Customer creation if he does not exists
-            {
-                sql = "insert into Customer ('PK_Email', 'Firstname', 'Lastname', 'Street', 'Town')" +
-                          "values ('" + customer.Email + "','" + customer.FirstName + "','" + customer.LastName +
-                          "','" + customer.Street + "','" + customer.Town + "')";
-
-                command = new SQLiteCommand(sql, dbConnection);
-                command.ExecuteNonQuery();
-            }
-        }
-
-        private void CheckOrderId()
-        {
-            /*Verification if the id of order exists*/
-            string sql = "select * from `Order` where PK_IDOrder='" + id + "'";
-
-            SQLiteCommand command = new SQLiteCommand(sql, dbConnection);
-            SQLiteDataReader reader = command.ExecuteReader();
-
-            if (reader.Read()) //if id does not exists
-            {
-                throw new Exception("This order id exists already");
-            }
+            components.Add(component);
         }
     }
 }
---
+++ KitBoxApp/KitBoxApp/Utils.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace KitBoxApp
+{
+    static class Utils
+    {
+        static private SQLiteConnection dbConnection = new SQLiteConnection("Data Source=db.sqlite;Version=3;");
+
+        static public void ExportToDatabase(Order order)
+        {
+            /*Start connection DataBase*/
+            dbConnection.Open();
+
+            Customer customer = order.Customer;
+
+            CheckCustomer(customer);
+            CheckOrderId(order);
+
+            /*If the customer and the id to order are good*/
+            string sql = "insert into `Order` ('PK_IDOrder', 'FK_Customer', 'FK_State', 'TotalPrice')" +
+                         "values ('" + order.Id + "','" + customer.Email + "','" + "1" + "','" + order.TotalPrice.ToString() + "')";
+
+            SQLiteCommand command = new SQLiteCommand(sql, dbConnection);
+            command.ExecuteNonQuery();
+
+
+            /*Add data inOrderComponentLink for each component located in components*/
+            foreach (Component component in order.Components)
+            {
+                sql = "insert into `OrderComponentLink` ('FK_Order', 'FK_Component')" +
+                         "values ('" + order.Id + "','" + component.code + "')";
+
+                command = new SQLiteCommand(sql, dbConnection);
+                command.ExecuteNonQuery();
+            }
+
+            /*End connection DataBase*/
+            dbConnection.Close();
+        }
+
+        static public Order ImportToDatabase(string id)
+        {
+            Order order = new Order(id);
+
+            /*Start connection DataBase*/
+            dbConnection.Open();
+
+
+            /*Recuperation to customer and TotalPrice data*/
+            string sql = "SELECT * FROM `Order`" +
+                         "INNER JOIN Customer ON `Order`.`FK_Customer`=`Customer`.`PK_Email`" +
+                         "WHERE `Order`.`PK_IDOrder`='" + order.Id + "'";
+
+            SQLiteCommand command = new SQLiteCommand(sql, dbConnection);
+
+            SQLiteDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                order.SetCustomer(reader["Pk_Email"].ToString(), reader["Firstname"].ToString(),
+                                        reader["Lastname"].ToString(), reader["Street"].ToString(),
+                                        reader["Town"].ToString());
+
+                order.TotalPrice = Convert.ToInt32(reader["TotalPrice"]);
+            }
+
+
+            /*Recuperation of components data*/
+            sql = "SELECT * FROM `OrderComponentLink`" +
+                  "INNER JOIN Component ON `OrderComponentLink`.`FK_Component`=`Component`.`Code`" +
+                  "WHERE OrderComponentLink.FK_Order='" + id + "'";
+
+            command = new SQLiteCommand(sql, dbConnection);
+
+            reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                order.AddComponent(new Component(reader["FK_Reference"].ToString(), reader["color"].ToString(),
+                                                 reader["code"].ToString(), Convert.ToInt32(reader["Price"]),
+                                                 4, true));
+            }
+
+
+            /*End connection DataBase*/
+            dbConnection.Close();
+
+            return order;
+        }
+
+
+        /*Private function*/
+        static private void CheckCustomer(Customer customer)
+        {
+            /*Verification if the customer exists*/
+            string sql = "select * from Customer where PK_Email='" + customer.Email + "'";
+
+            SQLiteCommand command = new SQLiteCommand(sql, dbConnection);
+            SQLiteDataReader reader = command.ExecuteReader();
+
+            if (!reader.Read()) //Customer creation if he does not exists
+            {
+                sql = "insert into Customer ('PK_Email', 'Firstname', 'Lastname', 'Street', 'Town')" +
+                          "values ('" + customer.Email + "','" + customer.FirstName + "','" + customer.LastName +
+                          "','" + customer.Street + "','" + customer.Town + "')";
+
+                command = new SQLiteCommand(sql, dbConnection);
+                command.ExecuteNonQuery();
+            }
+        }
+
+        static private void CheckOrderId(Order order)
+        {
+            /*Verification if the id of order exists*/
+            string sql = "select * from `Order` where PK_IDOrder='" + order.Id + "'";
+
+            SQLiteCommand command = new SQLiteCommand(sql, dbConnection);
+            SQLiteDataReader reader = command.ExecuteReader();
+
+            if (reader.Read()) //if id does not exists
+            {
+                throw new Exception("This order id exists already");
+            }
+        }
+    }
+}
