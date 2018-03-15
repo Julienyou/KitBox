a746a0717877feac00334aa79b4c9b8e46c0a920
Christophe Simon <c.christophesimon@gmail.com>
2018-03-15 21:32:51 +0100

    Update class DoorConstraint

--- KitBoxApp/KitBoxApp/DoorConstraint.cs
+++ KitBoxApp/KitBoxApp/DoorConstraint.cs
@@ -8,11 +8,10 @@ namespace KitBoxApp
 {
     class DoorConstraint : IConstraintChecker<Box>
     {
-        private bool knop;
         private List<string> colors;
         private List<Tuple<int,int>> doorDimensions;
 
-        public DoorConstraint(bool knop, List<string> colors, List<Tuple<int,int>> doorDimensions)
+        public DoorConstraint(List<string> colors, List<Tuple<int,int>> doorDimensions)
         {
             this.knop = knop;
             this.colors = colors;
