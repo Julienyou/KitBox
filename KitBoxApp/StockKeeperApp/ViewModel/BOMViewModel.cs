622320757b8b8351fd0d550b2f31c00104231c43
Guillaume <guillaumedemoff@gmail.com>
2018-06-10 22:55:46 +0200

    Add validate command

--- KitBoxApp/StockKeeperApp/ViewModel/BOMViewModel.cs
+++ KitBoxApp/StockKeeperApp/ViewModel/BOMViewModel.cs
@@ -1,10 +1,13 @@
-﻿using KitBox.Core.Model;
+﻿using KitBox.Core;
+using KitBox.Core.Model;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace StockKeeperApp.ViewModel
 {
@@ -17,20 +20,20 @@ namespace StockKeeperApp.ViewModel
         public JArray Components { get => JArray.FromObject(Order.Components);}
         #endregion
 
+        #region ICommands
+        public ICommand ValidateCommand
+        {
+            get
+            {
+                return new CommandHandler((x) => { ((Window)x).Close(); }, true);
+            }
+        }
+        #endregion
+
         #region constructor
         public BOMViewModel(Order order)
         {
             Order = order;
-            Order.Components = new List<Dictionary<string, string>>();
-            Order.Components.Add(new Dictionary<string, string>());
-            Order.Components[0]["Id"] = "Id1";
-            Order.Components[0]["Name"] = "Name1";
-            Order.Components[0]["IsChecked"] = "False";
-            Order.Components.Add(new Dictionary<string, string>());
-            Order.Components[1]["Id"] = "Id2";
-            Order.Components[1]["Name"] = "Name2";
-            Order.Components[0]["IsChecked"] = "True";
-
         }
         #endregion
     }
