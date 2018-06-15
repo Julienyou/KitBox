4a66ff38d0a74ca4a4d09c97adaba54c00a1d00f
Ludovic <ludo.m@live.fr>
2018-06-15 11:25:20 +0200

    Setup test project

---
+++ KitBoxApp/KitBox.test/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using KitBox.Core;
+using NUnit.Framework;
+using System.Data.SQLite;
+
+namespace KitBox.test
+{
+    [TestFixture()]
+    public class UtilsTest
+    {
+
+        [SetUp()]
+        public void Init()
+        {
+            Utils.DBConnection = new SQLiteConnection("db.sqlite");
+        }
+
+        [Test()]
+        public void ImportFromDatabase()
+        {
+            Utils.ImportFromDatabase("0");
+        }
+    }
+}
